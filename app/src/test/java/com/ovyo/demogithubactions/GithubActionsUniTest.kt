fc7893ea8753f15ed3e98f835cc9ae74ead2bfd7
dev-sharma-deltatre <dev.sharma@deltatre.com>
2025-06-30 11:53:01 +0530

    add test cases tests

---
+++ app/src/test/java/com/ovyo/demogithubactions/GithubActionsUniTest.kt
@@ -0,0 +1,12 @@
+package com.ovyo.demogithubactions
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GithubActionsUniTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(5, 2 + 2)
+    }
+}
